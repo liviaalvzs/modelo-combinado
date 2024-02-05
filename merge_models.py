9b6415da058e905f8cd09581581da9afd766cd9b
Livia <liv.alvessantana@gmail.com>
2024-02-05 15:17:39 -0300

    importa sklearn e arruma nome dos csv

--- merge_models.py
+++ merge_models.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import json
 from rich import print
+from sklearn.metrics import mean_squared_error
 
 # le arquivo com os MSE de todos os modelos (antes de combinar)
 with open('src/mse.txt', 'r') as file:
@@ -13,5 +14,5 @@ def calcular_mse(dataframe, coluna_real, coluna_predita):
     return mean_squared_error(dataframe[coluna_real], dataframe[coluna_predita])
 
 # le os modelos finais
-final_models = {subm: pd.read_csv(f'D:/modelo-combinado/src/final_model_{subm}.csv') for subm in submercados}
+final_models = {subm: pd.read_csv(f'D:/modelo-combinado/src/final_model{subm}.csv') for subm in submercados}
 
